5b960e3c6c97d6ff3e24f0e46b9c743c583acda2
Unknown <citizenkaneVII@gmail.com>
2017-09-07 01:03:56 -0600

    Finally got it!
    Do you know how unituitive this program is?!??

---
+++ Assets/Birds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birds : Animals {
+
+	public virtual void Start () {
+		Fly();
+		
+	}
+	
+	// These are declarations
+	void Fly() {
+		print(this.name + " Fly.");
+	}
+
+
+}
---
+++ Assets/Humans.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Humans : Mammals {
+
+public virtual void Start () {
+		Facebook();
+		
+	}
+	
+	// These are declarations
+	void Facebook() {
+		print(this.name + " Facebook.");
+	}
+
+
+}
