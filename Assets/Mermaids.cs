0bf454b19b0968132a4ae48e3c284f465ea9685b
Unknown <citizenkaneVII@gmail.com>
2017-09-07 01:32:09 -0600

    Merge branch 'feature/Classes' into develop
    Extra detail on mermaids

--- Assets/Mermaids.cs
+++ Assets/Mermaids.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //  My reasoing for this is that Mermaids have mammory glands.
+//  They also have hair.  I'll admit that I don't know if they give
+//  live birth or lay eggs.  But they sill have mammory glands,
+//  So their mammals!
 
 
 public class Mermaids : Mammals {
